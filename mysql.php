007d59096a9e08c04de9270714e4cc151237a081
Luis Munoz <cryptoweb@cryptorepair.com>
2020-05-20 15:31:49 -0400

    Basic: Connect to mysql db

---
+++ mysql.php
@@ -0,0 +1,26 @@
+<?php
+ 
+// CONNECT TO MYSQL SERVER
+
+// MYSQL Credentials 
+$servername = "localhost";
+$username = "";
+$password = "";
+$port = 3306;
+$db = "";
+
+// Create a connection using default port
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Create connection when using a different port 
+//$conn = new mysqli($servername.':'.$port, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error){
+  die ("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// QUERY YOUR DB
+
+?>
